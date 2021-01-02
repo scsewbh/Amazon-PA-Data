0a0a72b324866d1f3d69a427361a9d65d9f88ac8
Sanjay S <scsewbh@gmail.com>
2021-01-01 20:09:38 -0500

    Links Placed to Database

--- Products.py
+++ Products.py
@@ -11,13 +11,15 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 #mycursor.execute("CREATE DATABASE mydatabase")
-#mycursor.execute("CREATE TABLE Products (Links TEXT(255))")
+#mycursor.execute("CREATE TABLE products (id INT AUTO_INCREMENT PRIMARY KEY, PartialLink TEXT, Link TEXT)")
 
-mycursor.execute("SHOW TABLES")
-#mycursor.execute("DROP TABLE customers")
+#mycursor.execute("SHOW TABLES")
+#mycursor.execute("DROP TABLE products")
 
 #for x in mycursor:
 #  print(x)
+
+#mydb.commit()
 
 amzn_base_url = 'https://www.amazon.com'
 amzn_bestElec_url = 'https://www.amazon.com/Best-Sellers-Electronics/zgbs/electronics/'
@@ -27,13 +29,19 @@ class AMZN:
     def __init__(self):
         self.session = requests.Session()
 
-    def showResult(self):
+    def results(self):
+        sql = "INSERT INTO products (PartialLink, Link) VALUES (%s, %s)"
         test = self.session.get(amzn_bestElec_url)
         soup = bs(test.text, 'html.parser')
         a = soup.find_all('a', class_='a-link-normal a-text-normal')
+        data = []
         for x in a:
-            print("Found the URL:", x['href'])
+            data.append((x['href'], amzn_base_url + x['href']))
+        print(data)
+        mycursor.executemany(sql, data)
+        mydb.commit()
 
+        print(mycursor.rowcount, "was inserted.")
 
 x = AMZN()
-x.showResult()
+x.results()
