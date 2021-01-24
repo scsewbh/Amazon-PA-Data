741f39a8fa0af537c1f45be9ad25aced9f3ccf0b
Sanjay S <scsewbh@gmail.com>
2021-01-23 21:11:10 -0500

    Version 1 - Password Reveal

--- VariableUpdates.py
+++ VariableUpdates.py
@@ -13,9 +13,9 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 
 mydb = mysql.connector.connect(
-    host="35.231.149.95",
+    host="35.237.170.18",
     user="root",
-    password="Heroku3031",
+    password="Heroku3031*",
     database="mydatabase"
 )
 
